e105f9b201eb996726a34e3c4d104acde92ec57e
Lowell Bander <lowellbander@gmail.com>
2014-11-04 09:47:41 -0800

    found duplicates

--- find_duplicate.py
+++ find_duplicate.py
@@ -1,9 +1,12 @@
+"""find the duplicates in a list in constant time"""
+
 def find_duplicate(nums):
+    """find the duplicates in a list in constant time"""
     found = {}
     for num in nums:
-    	if num in found:
-    		return num
-    	else:
-    		found[num] = True
+        if num in found:
+            return num
+        else:
+            found[num] = True
     return None
 
