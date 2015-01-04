a01b5138f039366e14c2287c1f1eca4cba1706a9
Lowell Bander <lowellbander@gmail.com>
2015-01-03 20:37:56 -0800

    CTCI 4.5

---
+++ LCA.cpp
@@ -0,0 +1,20 @@
+template <typename T>
+struct Node {
+    T value;
+    Node* left;
+    Node* right;
+};
+ 
+// Cracking the Coding Interview, Question 4.5
+// Design an algorithm and write code to find the first common ancestor of two
+// nodes in a binary tree. Avoid storing additional nodes in a data structure.
+Node* LCA (Node* root, Node* p, Node* q) {
+    if (root == NULL) return NULL;
+    if (root == p || root == q) return root;
+    Node* L = LCA(root->left, p, q);
+    Node* R = LCA(root->right, p, q);
+    if (L && R) return root;
+    if (L != NULL) return L;
+    if (R != NULL) return R;
+    return NULL;
+}
